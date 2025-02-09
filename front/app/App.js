e437cbe30abf0de232e78bf232f0c559cfc617dc
wovlf02 <onlygithub02@gmail.com>
2025-02-09 20:10:21 +0900

    commit

--- front/app/App.js
+++ front/app/App.js
@@ -1,39 +1,70 @@
 import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-// 스크린 컴포넌트 가져오기
+import { Image } from 'react-native';
+import HomeScreen from './src/screens/home/HomeScreen';
+import CommunityScreen from './src/screens/community/CommunityMainScreen';
+import PersonalLearningScreen from './src/screens/personal/PersonalStudyMainScreen';
+import GroupLearningScreen from './src/screens/group/GroupStudyMainScreen';
+import MyPageScreen from './src/screens/mypage/MyPageMainScreen';
 import IntroScreen from './src/screens/IntroScreen';
 import LoginScreen from './src/screens/auth/LoginScreen';
 import RegisterScreen from './src/screens/auth/RegisterScreen';
 import FindAccountScreen from "./src/screens/auth/FindAccountScreen";
 import ResetPasswordScreen from "./src/screens/auth/ResetPasswordScreen";
-import HomeScreen from "./src/screens/home/HomeScreen";
 
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Intro" screenOptions={{ headerShown: false }}>
-                {/* 최초 인트로 화면 */}
-                <Stack.Screen name="Intro" component={IntroScreen} />
+const screenOptions = ({ route }) => ({
+    tabBarIcon: ({ focused, size }) => {
+        let iconPath;
+        switch (route.name) {
+            case 'Home':
+                iconPath = require('./src/assets/home.png');
+                break;
+            case 'Community':
+                iconPath = require('./src/assets/community.png');
+                break;
+            case 'Personal Learning':
+                iconPath = require('./src/assets/personal.png');
+                break;
+            case 'Group Learning':
+                iconPath = require('./src/assets/group.png');
+                break;
+            case 'My Page':
+                iconPath = require('./src/assets/mypage.png');
+                break;
+        }
+        return <Image source={iconPath} style={{ width: size, height: size, tintColor: focused ? '#007AFF' : '#8E8E93' }} />;
+    },
+    tabBarShowLabel: true,
+    tabBarActiveTintColor: '#007AFF',
+    tabBarInactiveTintColor: '#8E8E93',
+});
 
-                {/* 로그인 화면 */}
-                <Stack.Screen name="Login" component={LoginScreen} />
+const MainTabNavigator = () => (
+    <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Community" component={CommunityScreen} />
+        <Tab.Screen name="Personal Learning" component={PersonalLearningScreen} />
+        <Tab.Screen name="Group Learning" component={GroupLearningScreen} />
+        <Tab.Screen name="My Page" component={MyPageScreen} />
+    </Tab.Navigator>
+);
 
-                {/* 회원가입 화면 */}
-                <Stack.Screen name="Register" component={RegisterScreen} />
+const App = () => (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Intro" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Intro" component={IntroScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="FindAccount" component={FindAccountScreen} />
+            <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+            <Stack.Screen name="Home" component={MainTabNavigator} />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
 
-                {/* 계정 찾기 화면 */}
-                <Stack.Screen name="FindAccount" component={FindAccountScreen} />
-
-                {/* 비밀번호 재설정 화면 */}
-                <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
-
-                {/* 홈 화면 */}
-                <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-}
+export default App;
