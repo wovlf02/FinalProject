da343cab17e5ddbf4fa148141c4072bb8302e15b
wovlf02 <onlygithub02@gmail.com>
2025-05-08 18:00:51 +0900

    community 테스트 시작

--- back/src/main/java/com/hamcam/back/dto/community/post/request/PostCreateRequest.java
+++ back/src/main/java/com/hamcam/back/dto/community/post/request/PostCreateRequest.java
@@ -1,11 +1,14 @@
 package com.hamcam.back.dto.community.post.request;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * 게시글 작성 요청 DTO
  * <p>
- * 제목, 내용, 카테고리, 작성자 정보 등을 포함합니다.
+ * 제목, 내용, 카테고리, 작성자 정보, 첨부파일 목록을 포함합니다.
  * </p>
  */
 @Data
@@ -30,4 +33,9 @@ public class PostCreateRequest {
      * 게시글 카테고리
      */
     private String category;
+
+    /**
+     * 첨부파일 목록
+     */
+    private List<MultipartFile> attachments;
 }
